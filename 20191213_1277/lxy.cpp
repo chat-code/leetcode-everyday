a1df7b48f7e21ee1f4e337b5bfef05c674e6b687
liuxueyang <liuxueyang457@gmail.com>
2019-12-13 10:49:31 +0800

    lxy: optimize solution to 1277

--- 20191213_1277/lxy.cpp
+++ 20191213_1277/lxy.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 using VI = vector<int>;
 
-class Solution {
+class Solution1 {
 public:
   int countSquares(vector<vector<int>>& matrix) {
     int m = int(matrix.size()), n = int(matrix[0].size());
@@ -33,6 +33,32 @@ public:
 		      break;
 		    }
 		matrix[i][j] += min(min(len1, len2), len);
+	      }
+	    res += matrix[i][j];
+	  }
+      }
+
+    return res;
+  }
+};
+
+class Solution {
+public:
+  int countSquares(vector<vector<int>>& matrix) {
+    int m = int(matrix.size()), n = int(matrix[0].size());
+    int res = 0;
+    for (int i = 0; i < m; i++)
+      {
+	for (int j = 0; j < n; j++)
+	  {
+	    if (!matrix[i][j]) continue;
+	    int i1 = i - 1, j1 = j - 1;
+	    // 判断某一行或者某一列指定长度是不是都为一的方法优化
+	    // 如果都为一，那么以最右边或者最下面的那个点一定在一个正方形里面
+	    // 所以只需要得到正方形的边长即可
+	    if (i1 >= 0 && j1 >= 0 && matrix[i1][j1])
+	      {
+		matrix[i][j] += min(min(matrix[i - 1][j], matrix[i][j - 1]), matrix[i1][j1]);
 	      }
 	    res += matrix[i][j];
 	  }
