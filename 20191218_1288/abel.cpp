c8301d488f644127201fc1b625b159c3ec6cfe4b
liuxueyang <liuxueyang457@gmail.com>
2019-12-19 00:00:52 +0800

    abel finish solution to leetcode 1288

---
+++ 20191218_1288/abel.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <unordered_map>
+
+using namespace std;
+using VI = vector<int>;
+using PII = pair<int, int>;
+
+class Solution {
+public:
+  int removeCoveredIntervals(vector<vector<int>>& intervals) {
+    if (intervals.size() < 2) return int(intervals.size());
+
+    vector<PII> ps;
+    for (auto &x: intervals) ps.push_back(make_pair(x[0], x[1]));
+    sort(ps.begin(), ps.end());
+
+    unordered_map<int, int> bak;
+    VI far;
+    far.push_back(ps[0].second);
+    bak[ps[0].second] = ps[0].first;
+
+    for (int i = 1; i < int(ps.size()); i++)
+      {
+        int len = int(far.size());
+        auto st = lower_bound(far.begin(), far.end(), ps[i].second);
+        if (st == far.end())
+          {
+            if (bak[far[len-1]] == ps[i].first)
+                far[len-1] = ps[i].second;
+            else
+                far.push_back(ps[i].second);
+            bak[ps[i].second] = ps[i].first;
+          }
+      }
+    return int(far.size());
+  }
+};
+
+int main(void)
+{
+  Solution a;
+  vector<VI> v1 {{1,4},{3,6},{2,8}};
+  a.removeCoveredIntervals(v1);
+
+  return 0;
+}
