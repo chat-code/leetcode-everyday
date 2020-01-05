96b8f6f419f0307e7e0419ea6e0f63c327d1ebce
soulomoon <fwy996602672@gmail.com>
2020-01-06 03:49:19 +0800

    add pick feature: choose, random, week

--- utility/problemautopicker.py
+++ utility/problemautopicker.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass
 from random import choice
 from typing import Dict
 
-from utility.problempicker import get_past_problems, check_cwd, get_new_dir_name, create_readme
+from utility.problempicker import get_past_problems, check_cwd, get_new_dir_name, create_readme, commit_readme, \
+    README_TEMPLATE, README_NEW, SUB_DIRS, TRUE_ALIAS
 
 PROBLEM_URL_PREFIX = "https://leetcode.com/problems/"
 ALL_PROBLEM_URL = "https://leetcode.com/api/problems/all/"
@@ -27,7 +28,7 @@ class Problem:
 
     @classmethod
     def from_dict(cls, info: Dict):
-        dif = {1: 'easy', 2: 'medium', 3: 'hard'}[info['difficulty']['level']]
+        dif = {1: 'Easy', 2: 'Medium', 3: 'Hard'}[info['difficulty']['level']]
         stat = info['stat']
         return cls(stat['question_id'], stat['question__title'], PROBLEM_URL_PREFIX + stat['question__title_slug'], dif)
 
@@ -39,18 +40,44 @@ class Problem:
                     yield cls.from_dict(info)
 
 
-def random_pick():
-    SUB_DIRS = [".", "past"]
-    README_TEMPLATE = "./utility/README_template.md"
-    README_NEW = "READEME.md"
-    TRUE_ALIAS = ("", "t", "T", "True")
+def random_generate():
     check_cwd()
     dates, lc_ids = get_past_problems(SUB_DIRS)
     problems_to_do = list(filter(lambda x: x.pid not in lc_ids, Problem.from_file()))
     chosen: Problem = choice(problems_to_do)
     info = chosen.to_dict()
     dirname = get_new_dir_name(dates, info)
-    create_readme(README_TEMPLATE, README_NEW, dirname, info, TRUE_ALIAS, from_input=False)
+    return create_readme(README_TEMPLATE, README_NEW, dirname, info, from_input=False)
+
+
+def random_pick():
+    commit_readme(random_generate())
+
+
+def choose_pick():
+    check_cwd()
+    pid = int(input('choose problem id:'))
+    dates, lc_ids = get_past_problems(SUB_DIRS)
+    problems_to_do = list(filter(lambda x: x.pid not in lc_ids, Problem.from_file()))
+    if pid in (i.pid for i in problems_to_do):
+        chosen: Problem = next(i for i in problems_to_do if i.pid == pid)
+        info = chosen.to_dict()
+        dirname = get_new_dir_name(dates, info)
+        commit_readme(create_readme(README_TEMPLATE, README_NEW, dirname, info, from_input=False))
+    elif pid in lc_ids:
+        print("problem have been chosen")
+    else:
+        print("problem id is not in the database")
+
+
+def week_pick():
+    ps = [random_generate() for i in range(7)]
+    for p in ps:
+        print(p.get_path())
+    comfirmed = input(f"write to the above files and commit? [T]/F")
+    if comfirmed in TRUE_ALIAS:
+        for p in ps:
+            commit_readme(p, auto=True)
 
 
 if __name__ == "__main__":
