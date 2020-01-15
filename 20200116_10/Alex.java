160cd8b16e94f17228871b616631d56b6c5a1dc3
alex <changhuxxu@gmail.com>
2020-01-15 22:30:12 +0800

    alex finished 10

---
+++ 20200116_10/Alex.java
@@ -0,0 +1,14 @@
+class Alex{
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) return s.isEmpty();
+        boolean first_match = (!s.isEmpty() &&
+            (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+
+        if (p.length() >= 2 && p.charAt(1) == '*'){
+            return (isMatch(s, p.substring(2)) ||
+                (first_match && isMatch(s.substring(1), p)));
+        } else {
+            return first_match && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
