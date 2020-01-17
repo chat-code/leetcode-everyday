a423151b87b9ab237aec69661c549e37c6ced077
mcc4 <sardine323@gmail.com>
2020-01-17 16:38:55 +0800

    Create jordi.java

---
+++ 20200118_504/jordi.java
@@ -0,0 +1,9 @@
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int result=0;
+        for (int i = 0; i < timeSeries.length; i++){
+            if (i  != timeSeries.length-1 &&timeSeries[i] + duration -1 >= timeSeries[i+1])
+                result = timeSeries[i+1] - timeSeries[i] + result;
+            else result = duration + result;
+        }
+        return result;
+    }
