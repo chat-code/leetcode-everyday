515a061d80ce3ca44aecf44defbfa606ccc1f146
saigyouji-yuyuko <996506604@qq.com>
2020-01-18 03:45:06 +0800

    finish 212

--- 20200103_212/Saigyouji-Yuyuko.cpp
+++ 20200103_212/Saigyouji-Yuyuko.cpp
@@ -1,20 +1,68 @@
 #include <string>
-#include <algorithm>
+#include <array>
+#include <vector>
+struct tire
+{
+    tire()
+    {
+        memset(table.data(), 0, sizeof(tire *) * 26);
+    }
+    ~tire() = default;
+    ::std::array<tire *, 26> table;
+    int index = -1;
+};
+
 class Solution
 {
 public:
-    int lengthOfLongestSubstring(::std::string s)
+    ::std::vector<::std::vector<char>> board;
+    ::std::vector<::std::string> words;
+    ::std::vector<::std::string> findWords(::std::vector<::std::vector<char>> &board, ::std::vector<::std::string> &words)
     {
-        int mmp[300];
-        memset(mmp, -1, sizeof(mmp));
-        auto begin = 0;
-        auto maxx = 0;
-        for (auto i = 0; i != s.size(); ++i)
+
+        ::std::vector<::std::string> ans;
+        this->board = move(board);
+        this->words = move(words);
+        auto root = maketire();
+        for (auto i = 0; i != this->board.size(); ++i)
+            for (auto j = 0; j != this->board[i].size(); ++j)
+                find(root, ans, i, j);
+        return ans;
+    }
+    void find(tire *root, ::std::vector<::std::string> &ans, int x, int y)
+    {
+        auto it = board[x][y] - 'a';
+        if (board[x][y] == ' ' || root->table[it] == nullptr)
+            return;
+        root = root->table[it];
+        if (root->index != -1 && words[root->index].size() != 0)
+            ans.emplace_back(move(words[root->index]));
+        auto c = board[x][y];
+        board[x][y] = ' ';
+        if (x > 0)
+            find(root, ans, x - 1, y);
+        if (y > 0)
+            find(root, ans, x, y - 1);
+        if (x < board.size() - 1)
+            find(root, ans, x + 1, y);
+        if (y < board[x].size() - 1)
+            find(root, ans, x, y + 1);
+        board[x][y] = c;
+    }
+    tire *maketire()
+    {
+        auto root = new tire();
+        for (auto i = 0; i != words.size(); ++i)
         {
-            begin = ::std::max(begin, mmp[s[i]] + 1);
-            mmp[s[i]] = i;
-            maxx = ::std::max(maxx, i - begin + 1);
+            auto ptr = root;
+            for (auto ch : words[i])
+            {
+                if (ptr->table[ch - 'a'] == nullptr)
+                    ptr->table[ch - 'a'] = new tire();
+                ptr = ptr->table[ch - 'a'];
+            }
+            ptr->index = i;
         }
-        return maxx;
+        return root;
     }
 };
