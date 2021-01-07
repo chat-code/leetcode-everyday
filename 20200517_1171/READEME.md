7e3e4ea9094497f92bcb482f827968d8a5d09c25
liuxueyang <liuxueyang457@gmail.com>
2021-01-07 18:45:12 +0800

    add stats.sh

---
+++ 20200517_1171/READEME.md
@@ -0,0 +1,6 @@
+### Remove Zero Sum Consecutive Nodes from Linked List
+
+* id: 1171
+* date: 2020-05-17
+* link: https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list
+* level: Medium
---
+++ stats.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# find . -type f -name '*.*' | xargs basename | sort | uniq
+names=(duangduang Saigyouji-Yuyuko dd Alex Kamikakushi jordi abel)
+tmp_file="__tmp__file__stats"
+
+for name in ${names[@]}
+do
+    cnt=`find . -type f -name "$name.*" | wc -l | xargs`
+    printf "$cnt : $name\n" >> $tmp_file
+done
+sort -k 1 -n -r $tmp_file
+rm ./$tmp_file
