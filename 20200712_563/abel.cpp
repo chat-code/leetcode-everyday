d92e319b64029e3bca8259afdaf22d5c86bca10f
liuxueyang <liuxueyang457@gmail.com>
2021-01-12 11:29:47 +0800

    abel finish 563

---
+++ 20200712_563/abel.cpp
@@ -0,0 +1,113 @@
+//给定一个二叉树，计算 整个树 的坡度 。 
+//
+// 一个树的 节点的坡度 定义即为，该节点左子树的节点之和和右子树节点之和的 差的绝对值 。如果没有左子树的话，左子树的节点之和为 0 ；没有右子树的话也是一
+//样。空结点的坡度是 0 。 
+//
+// 整个树 的坡度就是其所有节点的坡度之和。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,3]
+//输出：1
+//解释：
+//节点 2 的坡度：|0-0| = 0（没有子节点）
+//节点 3 的坡度：|0-0| = 0（没有子节点）
+//节点 1 的坡度：|2-3| = 1（左子树就是左子节点，所以和是 2 ；右子树就是右子节点，所以和是 3 ）
+//坡度总和：0 + 0 + 1 = 1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [4,2,9,3,5,null,7]
+//输出：15
+//解释：
+//节点 3 的坡度：|0-0| = 0（没有子节点）
+//节点 5 的坡度：|0-0| = 0（没有子节点）
+//节点 7 的坡度：|0-0| = 0（没有子节点）
+//节点 2 的坡度：|3-5| = 2（左子树就是左子节点，所以和是 3 ；右子树就是右子节点，所以和是 5 ）
+//节点 9 的坡度：|0-7| = 7（没有左子树，所以和是 0 ；右子树正好是右子节点，所以和是 7 ）
+//节点 4 的坡度：|(3+5+2)-(9+7)| = |10-16| = 6（左子树值为 3、5 和 2 ，和是 10 ；右子树值为 9 和 7 ，和是 1
+//6 ）
+//坡度总和：0 + 0 + 0 + 2 + 7 + 6 = 15
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [21,7,14,1,1,2,2,3,3]
+//输出：9
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目的范围在 [0, 104] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 递归 
+// 👍 106 👎 0
+
+// 求子树节点和。过程中计算坡度即可。
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+#include <iostream>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+using VI = vector<int>;
+using VS = vector<string>;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res;
+
+    int dfs(TreeNode *root) {
+        if (root == nullptr) return 0;
+        int left = dfs(root->left), right = dfs(root->right);
+        int sum = left + right + root->val;
+        res += abs(left - right);
+        return sum;
+    }
+
+    int findTilt(TreeNode *root) {
+        res = 0;
+        dfs(root);
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
