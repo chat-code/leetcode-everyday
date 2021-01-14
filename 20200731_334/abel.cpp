beacf127b7216f30277a04525d2b6e33c7cbdeb4
liuxueyang <liuxueyang457@gmail.com>
2021-01-14 21:23:36 +0800

    abel finish 334

---
+++ 20200731_334/abel.cpp
@@ -0,0 +1,83 @@
+//给你一个整数数组 nums ，判断这个数组中是否存在长度为 3 的递增子序列。 
+//
+// 如果存在这样的三元组下标 (i, j, k) 且满足 i < j < k ，使得 nums[i] < nums[j] < nums[k] ，返回 true
+// ；否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3,4,5]
+//输出：true
+//解释：任何 i < j < k 的三元组都满足题意
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [5,4,3,2,1]
+//输出：false
+//解释：不存在满足题意的三元组 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [2,1,5,0,4,6]
+//输出：true
+//解释：三元组 (3, 4, 5) 满足题意，因为 nums[3] == 0 < nums[4] == 4 < nums[5] == 6
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 105 
+// -231 <= nums[i] <= 231 - 1 
+// 
+//
+// 
+//
+// 进阶：你能实现时间复杂度为 O(n) ，空间复杂度为 O(1) 的解决方案吗？ 
+// 👍 261 👎 0
+
+// 更新 small 为更小的值的时候，不影响结果；因为这意味着 mid 之前本来就有比 mid 更小的
+
+//leetcode submit region begin(Prohibit modification and deletion)
+#include <iostream>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+using VI = vector<int>;
+using VS = vector<string>;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int> &nums) {
+        int len = nums.size();
+        if (len < 3) return false;
+        int small = INT_MAX, mid = INT_MAX;
+
+        for (int i = 0; i < len; ++i) {
+            if (nums[i] > mid) {
+                if (small < mid) return true;
+            } else if (nums[i] < small) {
+                small = nums[i];
+            } else if (nums[i] > small & nums[i] < mid) {
+                mid = nums[i];
+            }
+        }
+        return false;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
