837b9b9b54137d5180eb0907dcb53ab457952b02
liuxueyang <liuxueyang457@gmail.com>
2021-02-19 16:02:45 +0800

    abel finish 237

---
+++ 20200914_237/abel.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  void deleteNode(ListNode* node) {
+    ListNode* t = node->next;
+    node->val = t->val;
+    node->next = t->next;
+  }
+};
