74f1d51afe1635c679baa65d04d45da058814f57
liuxueyang <liuxueyang457@gmail.com>
2021-02-22 15:07:19 +0800

    abel finish 513

---
+++ 20210202_513/abel.cpp
@@ -0,0 +1,73 @@
+//给定一个二叉树，在树的最后一行找到最左边的值。 
+//
+// 示例 1: 
+//
+// 
+//输入:
+//
+//    2
+//   / \
+//  1   3
+//
+//输出:
+//1
+// 
+//
+// 
+//
+// 示例 2: 
+//
+// 
+//输入:
+//
+//        1
+//       / \
+//      2   3
+//     /   / \
+//    4   5   6
+//       /
+//      7
+//
+//输出:
+//7
+// 
+//
+// 
+//
+// 注意: 您可以假设树（即给定的根节点）不为 NULL。 
+// Related Topics 树 深度优先搜索 广度优先搜索 
+// 👍 148 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res, row;
+
+    void dfs(TreeNode * root, int r) {
+        if (root == nullptr) return;
+        if (r > row) {
+            row = r; res = root->val;
+        }
+        dfs(root->left, r + 1);
+        dfs(root->right, r + 1);
+    }
+
+    int findBottomLeftValue(TreeNode* root) {
+        res = row = -1;
+        dfs(root, 0);
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
