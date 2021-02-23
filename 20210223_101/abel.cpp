5dbb48838ab11ee0f3d4f29fd49638cf9079f295
liuxueyang <liuxueyang457@gmail.com>
2021-02-23 16:22:11 +0800

    abel finish 101

---
+++ 20210223_101/abel.cpp
@@ -0,0 +1,59 @@
+//给定一个二叉树，检查它是否是镜像对称的。 
+//
+// 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+// 
+//
+// 
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+//
+//
+// 
+//
+// 进阶： 
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索 
+// 👍 1244 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool dfs(TreeNode* left, TreeNode* right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        if (left->val != right->val) return false;
+        return dfs(left->right, right->left) && dfs(left->left, right->right);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return dfs(root->left, root->right);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
