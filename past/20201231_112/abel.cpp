1c056c2e9d5c3da2158979256a5cd048b83a7e3c
liuxueyang <liuxueyang457@gmail.com>
2021-03-01 15:04:01 +0800

    abel finish 112

---
+++ past/20201231_112/abel.cpp
@@ -0,0 +1,73 @@
+//给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和
+// targetSum 。 
+//
+// 叶子节点 是指没有子节点的节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,2,3], targetSum = 5
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1,2], targetSum = 0
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数目在范围 [0, 5000] 内 
+// -1000 <= Node.val <= 1000 
+// -1000 <= targetSum <= 1000 
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 519 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int t;
+    bool dfs(TreeNode * r, int cur) {
+        if (!r) {
+            return false;
+        }
+        cur += r->val;
+        if (!r->left && !r->right) {
+            return cur == t;
+        }
+        return dfs(r->left, cur) || dfs(r->right, cur);
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) return false;
+        t = targetSum;
+        return dfs(root, 0);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
