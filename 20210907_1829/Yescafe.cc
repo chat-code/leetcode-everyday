95ca99af65b7691a914ec8f9da2880df6e61d89f
Ivan Chien <qyc027@gmail.com>
2021-09-07 14:01:42 +0800

    Yescafe update 1829

--- 20210907_1829/Yescafe.cc
+++ 20210907_1829/Yescafe.cc
@@ -2,6 +2,7 @@ class Solution {
 public:
   vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
     vector<int> ans;
+    ans.reserve(nums.size());
     int mask = (1 << maximumBit) - 1;
     int v = mask;
   
