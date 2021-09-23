13c9a03bea20274ae434e3f5fff173f93d49e297
liuxueyang <liuxueyang457@gmail.com>
2021-09-23 16:34:46 +0800

    abel finish 1332

---
+++ 20210815_1332/abel.cpp
@@ -0,0 +1,41 @@
+// Date: Thu Sep 23 16:23:02 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+class Solution {
+public:
+  int removePalindromeSub(string s) {
+    int n = s.size();
+    for (int i = 0, j = n - 1; i < j; ++i, --j) {
+      if (s[i] != s[j]) return 2;
+    }
+    return 1;
+  }
+};
