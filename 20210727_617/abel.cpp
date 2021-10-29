1d7b2c6969236dfccce363bf02ac948c74afebbc
liuxueyang <liuxueyang457@gmail.com>
2021-10-29 17:21:34 +0800

    abel finish 617

---
+++ 20210727_617/abel.cpp
@@ -0,0 +1,94 @@
+// Date: Fri Oct 29 17:10:55 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+typedef ListNode LN;
+typedef LN* LNP;
+typedef TreeNode TN;
+typedef TN* TNP;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  void dfs(TNP ro1, TNP ro2) {
+    if (ro1 && ro2) {
+      ro1->val += ro2->val;
+
+      auto l1 = ro1->left, r1 = ro1->right,
+        l2 = ro2->left, r2 = ro2->right;
+
+      if (l1 && l2) dfs(l1, l2);
+      else if (!l1) ro1->left = l2;
+
+      if (r1 && r2) dfs(r1, r2);
+      else if (!r1) ro1->right = r2;
+    }
+  }
+
+  TreeNode* mergeTrees(TreeNode* ro1, TreeNode* ro2) {
+    if (!ro1) return ro2;
+    if (!ro2) return ro1;
+
+    dfs(ro1, ro2);
+
+    return ro1;
+  }
+};
