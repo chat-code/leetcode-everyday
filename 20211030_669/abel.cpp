4464add39592b1298bc9cfe57a79050197e1e192
liuxueyang <liuxueyang457@gmail.com>
2021-10-30 10:40:20 +0800

    abel finish 669

---
+++ 20211030_669/abel.cpp
@@ -0,0 +1,93 @@
+// Date: Sat Oct 30 10:29:48 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+typedef ListNode LN;
+typedef LN* LNP;
+typedef TreeNode TN;
+typedef TN* TNP;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  bool in(int x, int low, int high) {
+    return x >= low && x <= high;
+  }
+
+  TreeNode* trimBST(TreeNode* ro, int low, int high) {
+    if (!ro) return ro;
+
+    auto x = ro->val;
+    auto l = ro->left, r = ro->right;
+
+    if (in(x, low, high)) {
+      ro->left = trimBST(l, low, high);
+      ro->right = trimBST(r, low, high);
+      return ro;
+    } else if (x < low) {
+      return trimBST(r, low, high);
+    } else if (x > high) {
+      return trimBST(l, low, high);
+    }
+
+    return ro;
+  }
+};
