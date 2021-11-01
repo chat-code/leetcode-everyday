5c91e5e60c5795a3399220d59dbee666dfc84ec3
liuxueyang <liuxueyang457@gmail.com>
2021-11-01 18:25:48 +0800

    abel finish 1005

---
+++ 20210717_1005/abel.cpp
@@ -0,0 +1,87 @@
+// Date: Mon Nov  1 18:18:39 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  int largestSumAfterKNegations(vector<int>& a, int k) {
+    int n = a.size();
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n && k; ++i) {
+      if (a[i] >= 0) break;
+      a[i] = -a[i]; k--;
+    }
+
+    int res {};
+    if (!k) {
+      for (auto &c : a) res += c;
+      return res;
+    }
+
+    int am = INF;
+    for (auto &c : a) {
+      am = min(am, abs(c));
+      res += c;
+    }
+
+    if (k & 1) {
+      res -= 2 * am;
+    }
+
+    return res;
+  }
+};
