5dbaa38b0e601f73449f5ec44e4233edcc216f37
liuxueyang <liuxueyang457@gmail.com>
2021-11-02 23:24:08 +0800

    abel finish 738

---
+++ 20210703_738/abel.cpp
@@ -0,0 +1,90 @@
+// Date: Tue Nov  2 23:06:47 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  bool check(string s) {
+    int n = s.size();
+    for (int i = 1; i < n; ++i) {
+      if (s[i] < s[i - 1]) return false;
+    }
+    return true;
+  }
+
+  int monotoneIncreasingDigits(int m) {
+    string s = to_string(m);
+    int n = s.size();
+    for (int i = 1; i <= n; ++i) {
+      int j = n - i;
+      string t = s.substr(j, i);
+      if (check(t)) continue;
+      if (s[j] == '0') {
+        for (int k = j; k < n; ++k) s[k] = '0';
+      } else {
+        s[j] -= 1;
+        for (int k = j + 1; k < n; ++k) s[k] = '9';
+      }
+    }
+
+    int res;
+    stringstream ss(s);
+    ss >> res;
+    return res;
+  }
+};
