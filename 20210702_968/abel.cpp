476818bbc8846fdb3a2ea3a762fad4c51b87377c
liuxueyang <liuxueyang457@gmail.com>
2021-11-02 23:56:50 +0800

    abel finish 968

---
+++ 20210702_968/abel.cpp
@@ -0,0 +1,104 @@
+// Date: Tue Nov  2 23:37:14 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int res;
+
+  int dfs(TNP ro) {
+    if (!ro) return 2;
+
+    auto l = ro->left, r = ro->right;
+    if (!l && !r) {
+      return 0;
+    }
+
+    auto l1 = dfs(l), r1 = dfs(r);
+    if (!l1 || !r1) {
+      res++;
+      return ro->val = 1;
+    }
+
+    if (l1 == 1 || r1 == 1) {
+      return ro->val = 2;
+    }
+
+    return ro->val = 0;
+  }
+
+  int minCameraCover(TreeNode* ro) {
+    res = 0;
+    dfs(ro);
+
+    if (!ro->val) res++;
+
+    return res;
+  }
+};
