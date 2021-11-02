f7898e2fb53734ab5f4a190ce3cc5cdd634c6d3c
liuxueyang <liuxueyang457@gmail.com>
2021-11-02 15:27:19 +0800

    abel finish 860

---
+++ 20210711_860/abel.cpp
@@ -0,0 +1,76 @@
+// Date: Tue Nov  2 15:18:14 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  bool lemonadeChange(vector<int>& a) {
+    int n = a.size(), c1 {}, c2 {};
+    for (int i = 0; i < n; ++i) {
+      if (a[i] == 5) c1++;
+      else if (a[i] == 10) c1--, c2++;
+      else {
+        if (c2) {
+          c2--; c1--;
+        } else c1 -= 3;
+      }
+      if (c1 < 0 || c2 < 0) return false;
+    }
+
+    return true;
+  }
+};
