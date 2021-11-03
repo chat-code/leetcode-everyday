deb1d18ce69fe4fcf0f4c5bb8a992e36cabf9109
liuxueyang <liuxueyang457@gmail.com>
2021-11-03 09:34:35 +0800

    abel finish 1

---
+++ 20210630_1/abel.cpp
@@ -0,0 +1,78 @@
+// Date: Wed Nov  3 09:31:11 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<int> twoSum(vector<int>& a, int ta) {
+    int n = a.size();
+    unordered_map<int, int> m;
+    VI res(2, -1);
+    for (int i = 0; i < n; ++i) {
+      int k = ta - a[i];
+      if (m.find(k) != m.end()) {
+        res[0] = i;
+        res[1] = m[k];
+        break;
+      }
+      m[a[i]] = i;
+    }
+
+    return res;
+  }
+};
