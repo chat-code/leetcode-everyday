0146b9cf4aeca517774fd63143328e5d198034b6
liuxueyang <liuxueyang457@gmail.com>
2021-11-03 09:13:33 +0800

    abel finish 142

---
+++ 20210701_142/abel.cpp
@@ -0,0 +1,97 @@
+// Date: Wed Nov  3 08:34:25 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *detectCycle(ListNode *he) {
+    if (!he) return he;
+    if (he->next == he) return he;
+    if (!he->next) return nullptr;
+
+    LN *p1, *p2, *du = new LN(0, he);
+    p1 = du->next;
+    p2 = p1->next;
+
+    while (p1 != p2 && p1 && p2) {
+      p1 = p1->next;
+      p2 = p2->next;
+      if (p2) p2 = p2->next;
+      else break;
+    }
+
+    if (p1 == p2) {
+      LN *cur = du;
+      while (cur != p1 && cur && p1) {
+        p1 = p1->next;
+        cur = cur->next;
+      }
+      return cur;
+    } else return nullptr;
+
+    return nullptr;
+  }
+};
