33407a95b2877ab00247ff23001fb884cc742efe
liuxueyang <liuxueyang457@gmail.com>
2021-11-03 18:45:08 +0800

    abel finish 1109

---
+++ 20210625_1109/abel.cpp
@@ -0,0 +1,75 @@
+// Date: Wed Nov  3 18:26:52 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<int> corpFlightBookings(vector<vector<int>>& a, int n) {
+    VI res(n, 0), d(n + 1, 0);
+    for (auto &v : a) {
+      int x = v[0] - 1, y = v[1] - 1, c = v[2];
+      d[x] += c;
+      d[y + 1] -= c;
+    }
+    res[0] = d[0];
+    for (int i = 1; i < n; ++i) {
+      res[i] = res[i - 1] + d[i];
+    }
+    return res;
+  }
+};
