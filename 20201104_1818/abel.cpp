1cba46d7521f435b5dd745614f00badd43982916
liuxueyang <liuxueyang457@gmail.com>
2021-11-05 23:25:22 +0800

    abel finish 1818

---
+++ 20201104_1818/abel.cpp
@@ -0,0 +1,88 @@
+// Date: Fri Nov  5 22:20:54 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  int minAbsoluteSumDiff(vector<int>& a, vector<int>& b) {
+    int cur = 0, idx = -1;
+    int n = a.size(), sum {};
+
+    VI a1 = a;
+    sort(a1.begin(), a1.end());
+    for (int i = 0; i < n; ++i) {
+      sum = (sum + abs(a[i] - b[i])) % MOD;
+
+      int l = 0, r = n - 1, mid;
+      while (l < r) {
+        mid = (l + r) / 2;
+        if (a1[mid] >= b[i]) r = mid;
+        else l = mid + 1;
+      }
+
+      int g1 = abs(a[i] - b[i]), g2 = abs(a1[r] - b[i]);
+      if (r) g2 = min(g2, abs(a1[r - 1] - b[i]));
+      if (g2 < g1) {
+        cur = max(cur, g1 - g2);
+      }
+    }
+
+    sum = (sum - cur + MOD) % MOD;
+    return sum;
+  }
+};
