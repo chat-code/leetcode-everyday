bdcdb59b7a79ff6d710584d211f931f0f2e0fe8d
liuxueyang <liuxueyang457@gmail.com>
2021-11-05 15:35:28 +0800

    abel finish 274

---
+++ 20201109_274/abel.cpp
@@ -0,0 +1,86 @@
+// Date: Fri Nov  5 15:05:14 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  VI a;
+  int n;
+
+  int check(int h) {
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+      if (a[i] >= h) cnt++;
+    }
+    return cnt;
+  }
+
+  int hIndex(vector<int>& _a) {
+    a = _a;
+    n = a.size();
+    int l = 0, r = 10000, mid;
+    while (l < r) {
+      mid = (l + r + 1) / 2;
+      int tmp = check(mid);
+      if (tmp >= mid) l = mid;
+      else if (tmp < mid) r = mid - 1;
+    }
+
+    return l;
+  }
+};
