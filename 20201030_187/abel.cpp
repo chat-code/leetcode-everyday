543ccb66703ac6a9e6be79d5df05a195babc3fc9
liuxueyang <liuxueyang457@gmail.com>
2021-11-07 22:41:40 +0800

    abel finish 187

---
+++ 20201030_187/abel.cpp
@@ -0,0 +1,80 @@
+// Date: Sun Nov  7 21:59:55 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<string> findRepeatedDnaSequences(string s) {
+    unordered_map<string, int> m;
+    vector<string> res;
+    int n = s.size();
+
+    for (int i = 0, j = 9; j < n; ++j) {
+      if (j - i + 1 == 10) {
+        m[s.substr(i, 10)]++;
+        ++i;
+      }
+    }
+
+    for (auto &p : m) {
+      if (p.second > 1) res.push_back(p.first);
+    }
+
+    return res;
+  }
+};
