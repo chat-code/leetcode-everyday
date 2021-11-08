04ad9dedbdda13e02e9263dad5a015ed357612c1
liuxueyang <liuxueyang457@gmail.com>
2021-11-09 00:21:36 +0800

    abel finish 437

---
+++ 20201027_437/abel.cpp
@@ -0,0 +1,96 @@
+// Date: Tue Nov  9 00:07:47 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int t, res;
+  unordered_map<int, int> m;
+
+  void dfs(TNP ro, int cur) {
+    if (!ro) return;
+    cur += ro->val;
+    int tmp = cur - t;
+    if (m[tmp]) res += m[tmp];
+
+    m[cur]++;
+
+    auto l = ro->left, r = ro->right;
+    dfs(l, cur);
+    dfs(r, cur);
+    m[cur]--;
+  }
+
+  int pathSum(TreeNode* ro, int _t) {
+    t = _t; res = 0;
+    m[0] = 1;
+    dfs(ro, 0);
+    return res;
+  }
+};
