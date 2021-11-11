c0590d16fbec6f466f6c432440ac98c9b7710064
liuxueyang <liuxueyang457@gmail.com>
2021-11-11 18:29:36 +0800

    abel finish 869

---
+++ 20201021_869/abel.cpp
@@ -0,0 +1,76 @@
+// Date: Thu Nov 11 18:25:18 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  set<string> s;
+
+  bool reorderedPowerOf2(int n) {
+    for (int i = 0; i < 31; ++i) {
+      int x = 1 << i;
+      auto y = to_string(x);
+      sort(y.begin(), y.end());
+      s.insert(y);
+    }
+
+    auto y = to_string(n);
+    sort(y.begin(), y.end());
+    return s.find(y) != s.end();
+  }
+};
