183c383c3b3ec2288a9a1ca3f5884b6d5d2375de
liuxueyang <liuxueyang457@gmail.com>
2021-11-11 17:54:58 +0800

    abel finish 938

---
+++ 20201022_938/abel.cpp
@@ -0,0 +1,100 @@
+// Date: Thu Nov 11 17:47:22 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int res;
+  int x, y;
+
+  void dfs(TNP ro) {
+    if (!ro) return;
+    auto l = ro->left, r = ro->right;
+    auto v = ro->val;
+
+    if (v >= x && v <= y) {
+      res += v;
+      dfs(l);
+      dfs(r);
+    } else if (v < x) {
+      dfs(r);
+    } else if (v > y) {
+      dfs(l);
+    }
+  }
+
+  int rangeSumBST(TreeNode* ro, int low, int high) {
+    res = 0;
+    x = low, y = high;
+
+    dfs(ro);
+
+    return res;
+  }
+};
