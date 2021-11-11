e7ecc6b2cd470feb5cd2261667d95ef0514db2e7
liuxueyang <liuxueyang457@gmail.com>
2021-11-11 18:41:58 +0800

    abel finish 783

---
+++ 20201020_783/abel.cpp
@@ -0,0 +1,102 @@
+// Date: Thu Nov 11 18:30:44 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int res;
+  VI tmp;
+
+  void dfs(TNP ro) {
+    if (!ro) return;
+
+    auto l = ro->left, r = ro->right;
+    auto x = ro->val;
+
+    if (!l && !r) {
+      tmp.push_back(x);
+      return;
+    }
+
+    if (l) dfs(l);
+    tmp.push_back(x);
+    if (r) dfs(r);
+  }
+
+  int minDiffInBST(TreeNode* ro) {
+    res = INF;
+    dfs(ro);
+
+    for (int i = 1; i < tmp.size(); ++i) {
+      res = min(res, tmp[i] - tmp[i - 1]);
+    }
+
+    return res;
+  }
+};
