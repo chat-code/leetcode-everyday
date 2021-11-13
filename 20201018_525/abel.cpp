3c1b57177802e13fc8e9db43ef1579b38ac57729
liuxueyang <liuxueyang457@gmail.com>
2021-11-13 16:41:15 +0800

    abel finish 525

---
+++ 20201018_525/abel.cpp
@@ -0,0 +1,84 @@
+// Date: Sat Nov 13 16:31:37 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  int findMaxLength(vector<int>& a) {
+    int n = a.size();
+    VI p(n + 1, 0);
+    for (int i = 0; i < n; ++i) {
+      if (!a[i]) a[i] = -1;
+      p[i + 1] = p[i] + a[i];
+    }
+
+    unordered_map<int, int> pos;
+    int res {};
+    pos[0] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+      if (pos.find(p[i]) == pos.end()) {
+        pos[p[i]] = i;
+      } else {
+        res = max(res, i - pos[p[i]]);
+      }
+    }
+
+    return res;
+  }
+};
