13a3a980a54939b879c842383c9055dacf97ec2f
liuxueyang <liuxueyang457@gmail.com>
2021-11-30 23:09:18 +0800

    abel finish 2068

---
+++ 20211120_2068/abel.cpp
@@ -0,0 +1,73 @@
+// Date: Tue Nov 30 23:07:05 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  bool checkAlmostEquivalent(string s1, string s2) {
+    map<char, int> m1, m2;
+    for (auto &c : s1) m1[c]++;
+    for (auto &c : s2) m2[c]++;
+
+    for (char c = 'a'; c <= 'z'; ++c) {
+      if (abs(m1[c] - m2[c]) > 3) return false;
+    }
+
+    return true;
+  }
+};
