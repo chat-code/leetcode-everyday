79ae2ac60f51b10e4b9f49ad667c4582a60a05ad
liuxueyang <liuxueyang457@gmail.com>
2021-11-30 23:04:58 +0800

    abel finish 2064

---
+++ 20211121_2064/abel.cpp
@@ -0,0 +1,90 @@
+// Date: Tue Nov 30 22:59:44 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  VI a;
+  int n, m;
+
+  bool check(int x) {
+    int res = 0;
+    for (int i = 0; i < m; ++i) {
+      res += a[i] / x;
+      if (a[i] % x) res++;
+    }
+
+    return res <= n;
+  }
+
+  int minimizedMaximum(int _n, vector<int>& _a) {
+    n = _n;
+    a = _a;
+
+    m = a.size();
+    int l = 1, r = 1e5 + 10, mid;
+
+    while (l < r) {
+      mid = (l + r) / 2;
+      if (check(mid)) r = mid;
+      else l = mid + 1;
+    }
+
+    return r;
+  }
+};
