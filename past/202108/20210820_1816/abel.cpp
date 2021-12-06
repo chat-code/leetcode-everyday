71ee1fe98837381906ac84785461bfc86f28b727
liuxueyang <liuxueyang457@gmail.com>
2021-12-06 22:59:51 +0800

    abel finish 1816

---
+++ past/202108/20210820_1816/abel.cpp
@@ -0,0 +1,78 @@
+// Date: Mon Dec  6 22:56:52 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  string truncateSentence(string s, int k) {
+    stringstream ss(s);
+
+    vector<string> v;
+    string tmp;
+    while (ss >> tmp) {
+      v.push_back(tmp);
+      k--;
+      if (!k) break;
+    }
+
+    string res = v[0];
+    for (int i = 1; i < v.size(); ++i) res = res + " " + v[i];
+
+    return res;
+  }
+};
