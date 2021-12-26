58052f5f3e2d95f7ee1f8db3b179ec419bdbcc2b
liuxueyang <liuxueyang457@gmail.com>
2021-12-26 23:15:47 +0800

    abel finish 1078

---
+++ 20211225_1078/abel.cpp
@@ -0,0 +1,80 @@
+// Date: Sun Dec 26 22:54:12 2021
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<string> findOcurrences(string v, string a, string b) {
+    vector<string> v1, res;
+    stringstream ss(v);
+
+    string t;
+    while (ss >> t) {
+      v1.push_back(t);
+    }
+
+    for (int i = 2; i < v1.size(); ++i) {
+      if (v1[i - 2] == a && v1[i - 1] == b) {
+        res.push_back(v1[i]);
+      }
+    }
+
+    return res;
+  }
+};
