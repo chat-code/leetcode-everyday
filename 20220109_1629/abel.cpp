2e8822c820bc95c0d9185480559cbe5de1fb26b6
liuxueyang <liuxueyang457@gmail.com>
2022-01-09 00:27:04 +0800

    abel finish 1629

---
+++ 20220109_1629/abel.cpp
@@ -0,0 +1,85 @@
+// Date: Sun Jan  9 00:16:52 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  char slowestKey(vector<int>& a, string b) {
+    int n = a.size();
+
+    char res = b[0];
+    int maxr = a[0];
+
+    for (int i = 1; i < n; ++i) {
+      int cur = a[i] - a[i - 1];
+
+      if (cur > maxr) {
+        res = b[i];
+        maxr = cur;
+      } else if (cur == maxr && b[i] > res) {
+        res = b[i];
+      }
+    }
+
+    return res;
+  }
+};
