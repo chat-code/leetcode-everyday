a8f4c4bb65c596f430f9ee38935486afcca9c8e1
liuxueyang <liuxueyang457@gmail.com>
2022-01-08 20:09:00 +0800

    abel finish 89

---
+++ 20220108_89/abel.cpp
@@ -0,0 +1,93 @@
+// Date: Sat Jan  8 19:44:48 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+const int N = 65537;
+bool vis[N];
+
+class Solution {
+public:
+  VI res;
+  int n;
+
+  void dfs(int cur) {
+    res.push_back(cur);
+    vis[cur] = true;
+
+    for (int i = 0; i < n; ++i) {
+      int tmp = 1 << i, ne = cur ^ tmp;
+      if (!vis[ne]) {
+        dfs(ne);
+        break;
+      }
+    }
+  }
+
+  vector<int> grayCode(int _n) {
+    memset(vis, false, sizeof vis);
+    res.clear();
+    n = _n;
+
+    dfs(0);
+
+    return res;
+  }
+};
