d3d52353d614a2f74c54f7c01f6f28d3dd2c5d44
liuxueyang <liuxueyang457@gmail.com>
2022-01-15 18:59:40 +0800

    abel finish 2129

---
+++ 20220106_2129/abel.cpp
@@ -0,0 +1,95 @@
+// Date: Sat Jan 15 17:53:10 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  string capital(string a) {
+    string res;
+    int n = a.size();
+
+    if (n <= 2) {
+      for (auto &c : a) res += tolower(c);
+      return res;
+    }
+
+    res += toupper(a[0]);
+    for (int i = 1; i < n; ++i) res += tolower(a[i]);
+
+    return res;
+  }
+
+  string capitalizeTitle(string a) {
+    stringstream ss(a);
+    string res, tmp;
+
+    ss >> tmp;
+    res = capital(tmp);
+
+    while (ss >> tmp) {
+      res += " " + capital(tmp);
+    }
+
+    return res;
+  }
+};
