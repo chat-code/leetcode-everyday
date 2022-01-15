2ce9e0ebb405e56de326f585a75b822c75c53cce
liuxueyang <liuxueyang457@gmail.com>
2022-01-15 17:02:09 +0800

    abel finish 2134

---
+++ 20220112_2134/abel.cpp
@@ -0,0 +1,88 @@
+// Date: Sat Jan 15 16:35:16 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  int minSwaps(vector<int>& a) {
+    int n = a.size(), cnt1 {};
+
+    for (auto &x : a) if (x) cnt1++;
+
+    if (!cnt1 || cnt1 == n) return 0;
+
+    int res = n, cur {};
+    for (int i = 0, j = 0; i < n + cnt1; ++i) {
+      int i1 = i % n;
+      if (a[i1]) cur++;
+
+      if (i >= cnt1) {
+        if (a[j++]) cur--;
+      }
+
+      res = min(res, cnt1 - cur);
+    }
+
+    return res;
+  }
+};
