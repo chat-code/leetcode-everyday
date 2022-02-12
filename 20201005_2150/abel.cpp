f954e81791711becd58c52bd2b4be493a98aee68
liuxueyang <liuxueyang457@gmail.com>
2022-02-12 11:44:26 +0800

    abel finish 2150

---
+++ 20201005_2150/abel.cpp
@@ -0,0 +1,85 @@
+// Date: Sun Jan 23 10:39:59 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<int> findLonely(vector<int>& a) {
+    int n = a.size();
+    sort(a.begin(), a.end());
+    VI res;
+
+    for (int i = 0; i < n; ++i) {
+      if (i + 1 < n) {
+        if (a[i + 1] == a[i] || a[i + 1] == a[i] + 1) continue;
+      }
+      if (i - 1 >= 0) {
+        if (a[i - 1] == a[i] || a[i - 1] == a[i] - 1) continue;
+      }
+
+      res.push_back(a[i]);
+    }
+
+    return res;
+  }
+};
