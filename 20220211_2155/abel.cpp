0505e788f999c5816e740ac09d51fad6da7d9604
liuxueyang <liuxueyang457@gmail.com>
2022-02-12 11:35:52 +0800

    abel finish 2155

---
+++ 20220211_2155/abel.cpp
@@ -0,0 +1,101 @@
+// Date: Sun Jan 30 10:34:04 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  vector<int> maxScoreIndices(vector<int>& a) {
+    VI res;
+    int n = a.size();
+    VI p0(n + 2, 0), p1(n + 2, 0);
+
+    p0[0] = (a[0] == 0);
+    for (int i = 1; i < n; ++i) p0[i] = p0[i - 1] + (a[i] == 0);
+
+    p1[n - 1] = (a[n - 1] == 1);
+    for (int i = n - 2; i >= 0; --i) p1[i] = p1[i + 1] + (a[i] == 1);
+
+    int ans = 0;
+    for (int i = 0; i <= n; ++i) {
+      int tmp = 0;
+      if (i == 0) tmp = p1[i];
+      else if (i == n) tmp = p0[i - 1];
+      else tmp = p0[i - 1] + p1[i];
+
+      ans = max(ans, tmp);
+    }
+
+    for (int i = 0; i <= n; ++i) {
+      int tmp = 0;
+      if (i == 0) tmp = p1[i];
+      else if (i == n) tmp = p0[i - 1];
+      else tmp = p0[i - 1] + p1[i];
+
+      if (tmp == ans) {
+        res.push_back(i);
+      }
+    }
+
+    return res;
+  }
+};
