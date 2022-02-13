8494f30ba7a58966b0d0d28f11abe89a307ee35c
liuxueyang <liuxueyang457@gmail.com>
2022-02-13 15:35:52 +0800

    abel finish 1189

---
+++ 20220213_1189/abel.cpp
@@ -0,0 +1,85 @@
+// Date: Sun Feb 13 15:24:00 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  int maxNumberOfBalloons(string a) {
+    int m[26] {};
+
+    for (auto &c : a) m[c - 'a']++;
+
+    string b = "ballon";
+    map<char, int> m1;
+    for (auto &c : b) m1[c]++;
+
+    int res = INF;
+    for (auto &p : m1) {
+      char ch = p.first;
+      int cnt = p.second;
+      res = min(res, m[ch - 'a'] / cnt);
+    }
+
+    return res;
+  }
+};
