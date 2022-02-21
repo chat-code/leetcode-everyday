7da4d061a469d5312c61ac25f21138c57612575e
liuxueyang <liuxueyang457@gmail.com>
2022-02-21 19:21:09 +0800

    abel finish 838

--- past/202011/20201113_838/abel.cpp
+++ past/202011/20201113_838/abel.cpp
@@ -1,53 +1,103 @@
+// Date: Mon Feb 21 19:00:52 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
 class Solution {
 public:
-  using VI = vector<int>;
-
   string pushDominoes(string a) {
     int n = a.size();
-    VI d(n, 0);
+    VI ts(n, INF);
+    string b = a;
 
     for (int i = 0; i < n; ++i) {
-      if (a[i] == 'L' || a[i] == 'R') d[i] = 1;
-    }
-
-    for (int i = n - 1; i >= 0; --i) {
-      if (a[i] == 'L' && d[i] == 1) {
-        int j = i - 1;
-        while (j >= 0 && a[j] == '.') {
-          a[j] = 'L';
-          d[j] = d[j + 1] + 1;
-          --j;
-        }
-        i = j + 1;
-      }
-    }
-
-    for (int i = 0; i < n; ++i) {
-      if (a[i] == 'R' && d[i] == 1) {
+      if (a[i] == '.') continue;
+      else if (a[i] == 'R') {
+        ts[i] = 0;
+        b[i] = 'R';
+        int cur = 1;
         int j = i + 1;
-        while (j < n && a[j] != 'R') {
-          int t = d[j - 1] + 1;
-          if (a[j] == '.') {
-            a[j] = 'R';
-            d[j] = t;
-            ++j;
-            continue;
-          }
-          if (t < d[j]) {
-            d[j] = t;
-            a[j] = 'R';
-            ++j;
-          } else if (t == d[j]) {
-            a[j] = '.';
-            ++j;
-          } else {
+        while (j < n && a[j] == '.') {
+          b[j] = 'R';
+          ts[j] = cur++;
+          ++j;
+        }
+      } else {
+        ts[i] = 0;
+        b[i] = 'L';
+        int cur = 1;
+        int j = i - 1;
+        while (j >= 0 && a[j] == '.' && cur <= ts[j]) {
+          if (cur == ts[j]) {
+            b[j] = '.';
             break;
           }
+          b[j] = 'L';
+          ts[j] = cur++;
+          --j;
         }
-        i = j - 1;
       }
     }
 
-    return a;
+    return b;
   }
 };
