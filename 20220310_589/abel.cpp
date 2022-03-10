8d40a0614f272593ce606e972e8017033dbd50cc
liuxueyang <liuxueyang457@gmail.com>
2022-03-10 11:23:31 +0800

    abel finish 589

---
+++ 20220310_589/abel.cpp
@@ -0,0 +1,103 @@
+// Date: Thu Mar 10 11:19:33 2022
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+/*
+// Definition for a Node.
+class Node {
+public:
+int val;
+vector<Node*> children;
+
+Node() {}
+
+Node(int _val) {
+val = _val;
+}
+
+Node(int _val, vector<Node*> _children) {
+val = _val;
+children = _children;
+}
+};
+*/
+
+class Solution {
+public:
+  VI res;
+
+  vector<int> preorder(Node* root) {
+    res = VI{};
+    dfs(root);
+    return res;
+  }
+
+  void dfs(Node *r) {
+    if (!r) return;
+    res.push_back(r->val);
+
+    for (auto &c : r->children) {
+      dfs(c);
+    }
+  }
+};
