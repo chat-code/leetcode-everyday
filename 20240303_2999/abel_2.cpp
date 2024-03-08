681b7fe020fa034179965b20a65bac891352d367
liuxueyang <liuxueyang457@gmail.com>
2024-03-08 23:29:24 +0800

    abel add another solution for 2999

---
+++ 20240303_2999/abel_2.cpp
@@ -0,0 +1,249 @@
+// Date: Thu Mar  7 21:46:18 2024
+
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V> void __print(const pair<T, V> &x) {
+  cerr << '{';
+  __print(x.first);
+  cerr << ", ";
+  __print(x.second);
+  cerr << '}';
+}
+template <typename T> void __print(const T &x) {
+  int f = 0;
+  cerr << '{';
+  for (auto &i : x)
+    cerr << (f++ ? ", " : ""), __print(i);
+  cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V> void _print(T t, V... v) {
+  __print(t);
+  if (sizeof...(v))
+    cerr << ", ";
+  _print(v...);
+}
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x " = " << x << endl;
+#define debug2(x, y) cout << #x " = " << x << " " #y " = " << y << endl;
+#define debug3(x, y, z)                                                        \
+  cout << #x " = " << x << " " #y " = " << y << " " #z " = " << z << endl;
+#define dbg(x...)                                                              \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = [";    \
+  _print(x);                                                                   \
+  cerr << "\e[39m" << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#define dbg(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+#endif
+// End of LeetCode
+
+ll dp[20];
+
+class Solution {
+public:
+  string s, po;
+  int len, top, k;
+
+  ll dfs(int pos, bool bound, bool number) {
+    if (pos == len) {
+      return number;
+    }
+
+    if (!bound && number && dp[pos] != -1)
+      return dp[pos];
+
+    int low = 0, up = top;
+    ll ans{};
+
+    if (bound)
+      ckmin(up, s[pos] - '0');
+
+    if (pos >= len - k) {
+      int cur = po[pos] - '0';
+
+      if (cur >= low && cur <= up) {
+        ans += dfs(pos + 1, bound && cur == s[pos] - '0', true);
+      } else {
+        ans = 0;
+      }
+      if (!bound && number)
+        dp[pos] = ans;
+
+      return ans;
+    }
+
+    if (!number) {
+      ans += dfs(pos + 1, false, false);
+      low = 1;
+    }
+
+    For1(i, low, up) { ans += dfs(pos + 1, bound && i == s[pos] - '0', true); }
+
+    if (!bound && number)
+      dp[pos] = ans;
+
+    return ans;
+  }
+
+  ll solve(ll n, int _top, string post) {
+    s = to_string(n);
+    len = SZ(s);
+    k = SZ(post);
+
+    if (len < k) {
+      s = string(k - len, '0') + s;
+      len = k;
+      po = post;
+    } else if (len >= k) {
+      po = string(len - k, '0') + post;
+    }
+
+    top = _top;
+    memset(dp, -1, sizeof dp);
+
+    return dfs(0, true, false);
+  }
+  long long numberOfPowerfulInt(long long start, long long finish, int limit,
+                                string s) {
+    ll tmp1 = solve(start - 1, limit, s), tmp2 = solve(finish, limit, s);
+    dbg(tmp1, tmp2);
+
+    return tmp2 - tmp1;
+  }
+};
+
+#ifdef _DEBUG
+int main(void) {
+#ifdef _DEBUG
+  freopen("abel.in", "r", stdin);
+#endif
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  Solution a;
+  auto res = a.numberOfPowerfulInt(1, 6000, 4, "124");
+  dbg(res);
+  res = a.numberOfPowerfulInt(15, 215, 6, "10");
+  dbg(res);
+  res = a.numberOfPowerfulInt(1000, 2000, 4, "3000");
+  dbg(res);
+
+  return 0;
+}
+
+#endif
