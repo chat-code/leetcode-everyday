48c4c1e4ad070ceb5f244efee991a4f094c13c9b
liuxueyang <liuxueyang457@gmail.com>
2024-03-18 22:46:08 +0800

    abel finish 2463

---
+++ past/202402/20240222_2463/abel.cpp
@@ -0,0 +1,221 @@
+// Date: Mon Mar 18 21:32:59 2024
+
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V> void __print(const pair<T, V> &x) {
+  cerr << '{';
+  __print(x.first);
+  cerr << ", ";
+  __print(x.second);
+  cerr << '}';
+}
+template <typename T> void __print(const T &x) {
+  int f = 0;
+  cerr << '{';
+  for (auto &i : x)
+    cerr << (f++ ? ", " : ""), __print(i);
+  cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V> void _print(T t, V... v) {
+  __print(t);
+  if (sizeof...(v))
+    cerr << ", ";
+  _print(v...);
+}
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x " = " << x << endl;
+#define debug2(x, y) cout << #x " = " << x << " " #y " = " << y << endl;
+#define debug3(x, y, z)                                                        \
+  cout << #x " = " << x << " " #y " = " << y << " " #z " = " << z << endl;
+#define dbg(x...)                                                              \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = [";    \
+  _print(x);                                                                   \
+  cerr << "\e[39m" << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#define dbg(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+#endif
+// End of LeetCode
+
+const int N = 110;
+ll d[N][N], ro[N];
+pair<ll, ll> b[N];
+
+class Solution {
+public:
+  long long minimumTotalDistance(vector<int> &ro_, vector<vector<int>> &fa) {
+    int n = SZ(ro_);
+    For1(i, 1, n) ro[i] = ro_[i - 1];
+    sort(ro + 1, ro + 1 + n);
+
+    int m = SZ(fa);
+    For(i, 0, m) b[i + 1] = PII(fa[i][0], fa[i][1]);
+    sort(b + 1, b + 1 + m);
+
+    memset(d, 0x3f, sizeof d);
+    d[0][0] = 0;
+
+    int tot = 0;
+    For1(i, 1, m) {
+      tot += b[i].f2;
+      int top = min(tot, n);
+      For1(j, 0, top) {
+        int len = min(ll(j), b[i].f2);
+        ll sum = 0;
+        For1(k, 0, len) {
+          if (k)
+            sum += llabs(ro[j - k + 1] - b[i].f1);
+          ckmin(d[i][j], d[i - 1][j - k] + sum);
+        }
+      }
+    }
+
+    return d[m][n];
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  Solution a;
+  VI ro{0, 4, 6};
+  vector<VI> fac{{2, 2}, {6, 2}};
+  auto res = a.minimumTotalDistance(ro, fac);
+  dbg(res);
+
+  ro = VI{1, -1};
+  fac = {{-2, 1}, {2, 1}};
+  res = a.minimumTotalDistance(ro, fac);
+  dbg(res);
+
+  ro = VI{789300819,  -600989788, 529140594, -592135328,
+          -840831288, 209726656,  -671200998};
+  fac = {{-865262624, 6}, {-717666169, 0}, {725929046, 2},  {449443632, 3},
+         {-912630111, 0}, {270903707, 3},  {-769206598, 2}, {-299780916, 4},
+         {-159433745, 5}, {-467185764, 3}, {849991650, 7},  {-292158515, 6},
+         {940410553, 6},  {258278787, 0},  {83034539, 2},   {54441577, 3},
+         {-235385712, 2}, {75791769, 3}};
+  res = a.minimumTotalDistance(ro, fac);
+  dbg(res);
+  return 0;
+}
+
+#endif
